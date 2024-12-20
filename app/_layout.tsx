e2913f7a825e856e42224caac34cc7e6771f1265
Matthew Picozzi <matt@mlbworkforce.com>
2024-12-20 13:11:16 +0530

    added some New things

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Stack>
-          <Stack.Screen name="index" options={{ title: 'Sign in', headerShown: false }} />
+          <Stack.Screen name="index" options={{ title: 'Get Started', headerShown: false }} />
+          <Stack.Screen name="SignIn" options={{ title: 'Sign in', headerShown: false }} />
           <Stack.Screen name="SignUp/index" options={{ title: 'Sign up', headerShown: false }} />
             <Stack.Screen name='(usertab)' options={{ headerShown: false }} />
         </Stack>
