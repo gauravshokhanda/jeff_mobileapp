3687d9ce5b9a2ceef996458c268e05fea27ac935
Gagan <gd885973@gmail.com>
2024-12-20 12:53:36 +0530

    add button

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
-import { View, Image } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 export default function Index() {
     return (
         <View className="flex-1">
-            <View className='flex-1 border items-center flex-row flex-wrap gap-2'>
+            <View className='flex-1 border items-center flex-row flex-wrap gap-2 h-[100%]'>
                 {/* first */}
-                <View className="rounded-lg h-[20%] w-[46%] border border-sky-500 ml-3">
+                <View className="rounded-lg h-[45%] w-[46%]  border-sky-500 ml-3">
                     <Image
                         source={require("../assets/images/homescreen/zac-gudakov-UPbYh3A5cdg-unsplash.jpg")}
                         resizeMode="cover"
@@ -14,7 +14,7 @@ export default function Index() {
                     />
                 </View>
                 {/* second */}
-                <View className="rounded-lg h-[20%] w-[46%] border border-sky-500">
+                <View className="rounded-lg h-[45%] w-[46%] border border-sky-500">
                     <Image
                         source={require("../assets/images/homescreen/zac-gudakov-UPbYh3A5cdg-unsplash.jpg")}
                         resizeMode="cover"
@@ -22,7 +22,7 @@ export default function Index() {
                     />
                 </View>
                 {/* third */}
-                <View className="rounded-lg h-[20%] w-[46%] border border-sky-500 ml-3">
+                <View className="rounded-lg h-[45%] w-[46%] border border-sky-500 ml-3">
                     <Image
                         source={require("../assets/images/homescreen/zac-gudakov-UPbYh3A5cdg-unsplash.jpg")}
                         resizeMode="cover"
@@ -30,16 +30,23 @@ export default function Index() {
                     />
                 </View>
                 {/* fourth */}
-                <View className="rounded-lg h-[20%] w-[46%] border border-sky-500">
+                <View className="rounded-lg h-[45%] w-[46%] border border-sky-500">
                     <Image
                         source={require("../assets/images/homescreen/zac-gudakov-UPbYh3A5cdg-unsplash.jpg")}
                         resizeMode="cover"
                         style={{ width: "100%", height: "100%" }}
                     />
                 </View>
-            
-            </View>
 
+            </View>
+            <View className='flex-1'>
+            <View className="items-center justify-center mt-6">
+                <TouchableOpacity
+                    className="text-center rounded-3xl  px-10 bg-sky-500 ">
+                    <Text className="text-center mx-10 my-3 text-lg">Get Started</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
         </View>
     );
 }
