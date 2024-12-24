42c4d3ecb531ebf2ca924f39bd3cea63fb841e13
Matthew Picozzi <matt@mlbworkforce.com>
2024-12-24 12:22:40 +0530

    pushed

--- app/SignIn.jsx
+++ app/SignIn.jsx
@@ -11,7 +11,7 @@ import { useSelector } from 'react-redux';
 import Logo from '../assets/images/AC5D_Logo.jpg';
 
 export default function SignIn() {
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState()
   const [password, setPassword] = useState("")
   const [error, setError] = useState(null)
   const router = useRouter();
