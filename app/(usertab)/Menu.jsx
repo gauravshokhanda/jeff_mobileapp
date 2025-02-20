a32f1380e49fee3f72f938720bc187943ab3698f
gagandeep <gd885973@gmail.com>
2025-02-20 15:16:57 +0530

    update user screen

--- app/(usertab)/Menu.jsx
+++ app/(usertab)/Menu.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text, Image, Platform, TouchableOpacity,Alert } from "react-native";
+import { View, Text, Image, Platform, TouchableOpacity, Alert, Dimensions, SafeAreaView } from "react-native";
 import Svg, { Path } from "react-native-svg";
-import { useSelector ,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "expo-router"; // ✅ Use router for navigation
 import Icon from "react-native-vector-icons/FontAwesome5";
 import Box from "../../assets/images/MD.png";
 import { setLogout } from "../../redux/slice/authSlice";
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 // Object Array for menu items
 const imageData = [
@@ -21,9 +23,11 @@ const imageData = [
 
 
 const MenuHeader = () => {
+  const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+  const postContentWidth = screenWidth * 0.92;
   const userName = useSelector((state) => state.auth.user);
   const router = useRouter(); // ✅ Use router for navigation
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
 
   // const handlePress = (screen) => {
   //   if (screen) {
@@ -47,53 +51,67 @@ const MenuHeader = () => {
       router.push(screen);
     }
   };
-  
+
 
 
 
   return (
-    <View className={`bg-white h-full relative ${Platform.OS === "ios" ? "mt-16" : ""}`}>
-      {/* Header with User Info */}
-      <View className="bg-sky-950 h-56">
+    <SafeAreaView className="flex-1 bg-gray-200">
+
+      <LinearGradient
+        colors={['#082f49', 'transparent']}
+        className="h-[40%]"
+      >
         <View className="mt-10 px-4 gap-2 flex-row items-center">
           <Image
-            source={{ uri: "https://via.placeholder.com/50" }}
+            source={{ uri: "https://xsgames.co/randomusers/assets/avatars/male/74.jpg" }}
             className="w-14 h-14 border-2 border-white rounded-full"
           />
           <View className="gap-1">
-            <Text className="text-3xl font-semibold text-white">
+            <Text className="text-2xl font-semibold text-white">
               Welcome! {userName?.name || "User"}
             </Text>
+            <Text className="text-gray-400">📍 Florida, USA</Text>
           </View>
         </View>
 
-        {/* Wave Effect */}
-        <Svg className="absolute bottom-0 left-0 w-full h-16" viewBox="0 0 1440 360">
-          <Path
-            fill="#ffffff"
-            d="M0,200L80,190C160,180,320,160,480,170C640,180,800,220,960,220C1120,220,1280,180,1360,160L1440,140V320H0Z"
-          />
-        </Svg>
-      </View>
 
-      {/* Grid of Menu Items */}
-      <View className="flex flex-wrap flex-row mt-10 justify-center gap-4 p-4">
-        {imageData.map((item) => (
-          <TouchableOpacity
-            key={item.id}
-            activeOpacity={0.7}
-            className="relative w-48 h-28 flex items-center justify-center"
-            onPress={() => handlePress(item.screen)}
-          >
-            <Image source={item.source} className="w-full h-full absolute" />
-            <View className="absolute flex items-center">
-              <Icon name={item.icon} size={24} color="white" />
-              <Text className="text-white text-lg font-semibold mt-1">{item.label}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
+      </LinearGradient>
+      <View
+       className="flex-1 rounded-3xl bg-white"
+        style={{
+          marginTop: -screenHeight * 0.20,
+          width: postContentWidth,
+          marginHorizontal: (screenWidth - postContentWidth) / 2,
+          overflow: 'hidden'
+        }}
+      >
+        <View className="flex flex-wrap flex-row mt-10 justify-center gap-4 p-4">
+          {imageData.map((item) => (
+            <TouchableOpacity
+              key={item.id}
+              activeOpacity={0.7}
+              className="relative  h-28 flex items-center justify-center"
+              onPress={() => handlePress(item.screen)}
+              style={{ width: screenWidth * 0.40 }}
+            >
+              <Image source={item.source} className="w-full h-full absolute" />
+              <View className="absolute flex items-center">
+                <Icon name={item.icon} size={24} color="white" />
+                <Text className="text-white text-lg font-semibold mt-1">{item.label}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+
+
+
+
+
+
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
