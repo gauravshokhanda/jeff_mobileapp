e8506086c5326b8081f753dd045747c93e2eb406
abhijeetsodlan <abhijeetsodlan7@gmail.com>
2025-02-26 22:51:18 +0530

    pushing for streak

--- app/(usertab)/MapScreen.jsx
+++ app/(usertab)/MapScreen.jsx
@@ -47,7 +47,7 @@ export default function MapScreen() {
 
   const handleZoomIn = () => {
     if (location) {
-      setLocation((prevLocation) => ({
+      setLocation((prevLocation) => ({ 
         ...prevLocation,
         latitudeDelta: prevLocation.latitudeDelta / 2,
         longitudeDelta: prevLocation.longitudeDelta / 2,
