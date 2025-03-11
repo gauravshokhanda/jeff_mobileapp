85ae7b7c1b7166a4cc14a8c65917faf3eede8521
abhijeetsodlan <abhijeetsodlan7@gmail.com>
2025-03-11 11:57:45 +0530

    fixed the changes

--- app/(RealstateContractorTab)/MyListing.jsx
+++ app/(RealstateContractorTab)/MyListing.jsx
@@ -1,27 +1,40 @@
-import { View, Dimensions, Text, TextInput, FlatList, TouchableOpacity, Image, KeyboardAvoidingView, Platform, ScrollView, Alert, SafeAreaView, ActivityIndicator } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
-import { router } from 'expo-router';
-import { useSelector } from 'react-redux';
-import { API, baseUrl } from '../../config/apiConfig';
-import Swiper from 'react-native-swiper';
+import {
+  View,
+  Dimensions,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Alert,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import { useSelector } from "react-redux";
+import { API, baseUrl } from "../../config/apiConfig";
+import Swiper from "react-native-swiper";
 export default function Listing() {
-  const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+  const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
   const postContentWidth = screenWidth * 0.92;
 
-  const [selectedPropertyType, setSelectedPropertyType] = useState('residential');
+  const [selectedPropertyType, setSelectedPropertyType] =
+    useState("residential");
 
   const [properties, setProperties] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
 
   const token = useSelector((state) => state.auth.token);
   const userId = useSelector((state) => state.auth.user.id);
-
-
 
   const fetchProperties = async (page = 1) => {
     if (loading || page > totalPages) return;
@@ -38,7 +51,9 @@ export default function Listing() {
       // console.log("response", response.data.properties.last_page)
       // console.log("response", response.data.properties)
       setProperties((prev) =>
-        page === 1 ? response.data.properties.data : [...prev, ...response.data.properties.data]
+        page === 1
+          ? response.data.properties.data
+          : [...prev, ...response.data.properties.data]
       );
       setCurrentPage(response.data.properties.current_page);
       setTotalPages(response.data.properties.last_page);
@@ -58,42 +73,43 @@ export default function Listing() {
     return () => clearTimeout(delayDebounceFn); // Cleanup timeout
   }, [searchQuery, selectedPropertyType]);
 
-
   const loadMore = () => {
     if (currentPage < totalPages) {
       fetchProperties(currentPage + 1);
     }
   };
 
-
-
   const propertyTypes = [
-    { id: 'residential', label: 'Residential' },
-    { id: 'commercial', label: 'Commercial' },
+    { id: "residential", label: "Residential" },
+    { id: "commercial", label: "Commercial" },
   ];
 
   const renderPropertyTypeItem = ({ item }) => {
-    const isSelected = selectedPropertyType === item.id
+    const isSelected = selectedPropertyType === item.id;
     return (
       <TouchableOpacity
-        className={`px-8 py-2 flex-row items-center justify-center border-b-2 ${isSelected ? "border-sky-900" : "border-gray-300"}`}
+        className={`px-8 py-2 flex-row items-center justify-center border-b-2 ${
+          isSelected ? "border-sky-900" : "border-gray-300"
+        }`}
         onPress={() => setSelectedPropertyType(item.id)}
       >
-        <Text className={`text-lg font-medium ${isSelected ? "text-sky-900" : "text-gray-400"}`}>
+        <Text
+          className={`text-lg font-medium ${
+            isSelected ? "text-sky-900" : "text-gray-400"
+          }`}
+        >
           {item.label}
         </Text>
       </TouchableOpacity>
-    )
-  }
-
+    );
+  };
 
   const renderListening = ({ item }) => {
     const propertyImages = JSON.parse(item.property_images) || [];
 
     return (
       <View
-        style={{
-        }}
+        style={{}}
         className="rounded-xl bg-white shadow-md overflow-hidden mb-4"
       >
         <TouchableOpacity
@@ -108,19 +124,23 @@ export default function Listing() {
                 <Text className="text-white font-bold ml-2">{item.city}</Text>
               </View>
               <View className="flex-row gap-2">
-                <View className="bg-white rounded-full px-3 justify-center items-center"
-                >
-                  <Text className="text-slate-700"
+                <View className="bg-white rounded-full px-3 justify-center items-center">
+                  <Text
+                    className="text-slate-700"
                     style={{ fontSize: screenWidth * 0.03 }}
-                  >{item.house_type}
+                  >
+                    {item.house_type}
                   </Text>
                 </View>
                 <View className="bg-white rounded-full p-1 justify-center items-center">
-                  <Text className="text-slate-700"
+                  <Text
+                    className="text-slate-700"
                     style={{ fontSize: screenWidth * 0.03 }}
                   >
-                    {(item.locale.split(" ").slice(0, 2).join(" ") + (item.locale.split(" ").length > 2 ? "..." : "")).trim()}
-
+                    {(
+                      item.locale.split(" ").slice(0, 2).join(" ") +
+                      (item.locale.split(" ").length > 2 ? "..." : "")
+                    ).trim()}
                   </Text>
                 </View>
               </View>
@@ -151,9 +171,9 @@ export default function Listing() {
                         <Image
                           className="rounded-full"
                           source={{
-                            uri: `${baseUrl}${image.replace(/\\/g, '/')}`,
+                            uri: `${baseUrl}${image.replace(/\\/g, "/")}`,
                           }}
-                          style={{ width: '100%', height: '100%' }}
+                          style={{ width: "100%", height: "100%" }}
                           resizeMode="cover"
                         />
                       </View>
@@ -162,7 +182,7 @@ export default function Listing() {
                 ) : (
                   <Image
                     source={require("../../assets/images/realState/checkoutProperty.png")}
-                    style={{ width: '100%', height: '100%', borderRadius: 10 }}
+                    style={{ width: "100%", height: "100%", borderRadius: 10 }}
                     resizeMode="cover"
                   />
                 )}
@@ -171,7 +191,9 @@ export default function Listing() {
               <View className="flex-1 space-y-2 mt-2">
                 <View className="flex-row items-center justify-start">
                   <View className="flex-row items-center justify-center gap-2">
-                    <Text className="text-white text-2xl font-bold">${item.price}</Text>
+                    <Text className="text-white text-2xl font-bold">
+                      ${item.price}
+                    </Text>
                     <Text className="text-gray-300 text-lg">USD</Text>
                   </View>
                 </View>
@@ -179,18 +201,23 @@ export default function Listing() {
                 <View className="flex-row items-center">
                   <Ionicons name="calendar-outline" size={16} color="white" />
                   <Text className="text-gray-300 ml-2">
-                    Available from {new Date(item.available_from).toISOString().split('T')[0]}
+                    Available from{" "}
+                    {new Date(item.available_from).toISOString().split("T")[0]}
                   </Text>
                 </View>
 
                 <View className="flex-row items-center">
                   <Ionicons name="resize-outline" size={16} color="white" />
-                  <Text className="text-gray-300 ml-2">Area - {item.area} sq ft</Text>
+                  <Text className="text-gray-300 ml-2">
+                    Area - {item.area} sq ft
+                  </Text>
                 </View>
 
                 <View className="flex-row items-center">
                   <Ionicons name="bed-outline" size={16} color="white" />
-                  <Text className="text-gray-300 ml-2">{item.furnish_type}</Text>
+                  <Text className="text-gray-300 ml-2">
+                    {item.furnish_type}
+                  </Text>
                 </View>
               </View>
             </View>
@@ -199,7 +226,9 @@ export default function Listing() {
 
         {/* Bottom Section with Button */}
         <View className="bg-gray-200 rounded-b-2xl p-4 flex-row justify-between items-center">
-          <Text className="text-lg font-semibold text-gray-600">Get ready list of buyers</Text>
+          <Text className="text-lg font-semibold text-gray-600">
+            Get ready list of buyers
+          </Text>
           <TouchableOpacity className="bg-white px-4 py-2 rounded-lg shadow-md">
             <Text className="text-indigo-900 font-semibold">Update</Text>
           </TouchableOpacity>
@@ -208,15 +237,13 @@ export default function Listing() {
     );
   };
 
-
   return (
     <SafeAreaView className="flex-1 bg-gray-200">
-
       <LinearGradient
-        colors={['#082f49', 'transparent']}
-
+        colors={["#082f49", "transparent"]}
         style={{ height: screenHeight * 0.4 }}
       >
+        {/* Header */}
         <View className="mt-2">
           <TouchableOpacity
             className="absolute top-6 z-10 left-5"
@@ -225,43 +252,48 @@ export default function Listing() {
             <Ionicons name="arrow-back" size={24} color="white" />
           </TouchableOpacity>
           <Text className="text-2xl font-semibold text-white mb-4 py-4 text-center">
-            My property Listing
+            My Property Listing
           </Text>
         </View>
-        <View className="mx-5 mt-5 items-end">
-          <View className="bg-gray-100  h-12 mr-5 rounded-full px-3 flex-row items-center justify-between ">
+
+        {/* Search Bar */}
+        <View className="mx-5" style={{ marginTop: screenHeight * 0.02 }}>
+          <View
+            className="bg-gray-100 rounded-full flex-row items-center justify-between"
+            style={{
+              height: screenHeight * 0.06,
+              paddingHorizontal: screenWidth * 0.04,
+            }}
+          >
             <Ionicons name="search" size={18} color="black" />
             <TextInput
-
               placeholder="Search by City"
               placeholderTextColor={"gray"}
-              style={{ fontSize: 14 }}
-              className="flex-1 ml-5 text-lg text-sm"
+              className="flex-1 text-lg"
+              style={{ fontSize: 14, marginLeft: screenWidth * 0.03 }}
               value={searchQuery}
               onChangeText={setSearchQuery}
             />
             <Ionicons name="filter-sharp" size={26} color="black" />
-
           </View>
         </View>
-
-
       </LinearGradient>
 
-
-      <View className="rounded-3xl"
+      {/* Content Section */}
+      <View
+        className="rounded-3xl"
         style={{
-          position: 'absolute',
-          top: screenHeight * 0.20,
+          position: "absolute",
+          top: screenHeight * 0.22, // Adjusted to prevent overlap
           width: postContentWidth,
           height: screenHeight * 0.75,
           left: (screenWidth - postContentWidth) / 2,
-          backgroundColor: 'white',
-
+          backgroundColor: "white",
+          paddingTop: screenHeight * 0.02, // Added extra spacing
         }}
       >
-
         <View className="flex-1 m-5">
+          {/* Property Types */}
           <View className="p-3 rounded-lg">
             <FlatList
               data={propertyTypes}
@@ -272,8 +304,10 @@ export default function Listing() {
               contentContainerStyle={{ flexGrow: 1, gap: 10 }}
             />
           </View>
-          <View className=" flex-1 mt-2 mb-3">
-            {loading == true ? (
+
+          {/* Property Listings */}
+          <View className="flex-1 mt-2 mb-3">
+            {loading ? (
               <ActivityIndicator size="large" color="#082f49" />
             ) : (
               <FlatList
@@ -285,8 +319,6 @@ export default function Listing() {
                 onEndReachedThreshold={0.5}
               />
             )}
-
-
           </View>
         </View>
       </View>
