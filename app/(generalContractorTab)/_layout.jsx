4bb58bd7c787f751eede7fb99883db4e6025dfb9
abhijeetsodlan <abhijeetsodlan7@gmail.com>
2025-03-11 10:23:06 +0530

    new commits

--- app/(generalContractorTab)/_layout.jsx
+++ app/(generalContractorTab)/_layout.jsx
@@ -6,14 +6,17 @@ import { useFocusEffect } from "@react-navigation/native";
 import { useState, useEffect, useCallback } from "react";
 import { BackHandler, Keyboard } from "react-native";
 
-
 export default function TabRoot() {
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
 
   // Detect keyboard visibility
   useEffect(() => {
-    const showSubscription = Keyboard.addListener("keyboardDidShow", () => setKeyboardVisible(true));
-    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => setKeyboardVisible(false));
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () =>
+      setKeyboardVisible(true)
+    );
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () =>
+      setKeyboardVisible(false)
+    );
 
     return () => {
       showSubscription.remove();
@@ -31,17 +34,17 @@ export default function TabRoot() {
         }
         return false; // Allow default behavior (exit app)
       };
-  
+
       BackHandler.addEventListener("hardwareBackPress", onBackPress);
-      return () => BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+      return () =>
+        BackHandler.removeEventListener("hardwareBackPress", onBackPress);
     }, [])
   );
-  
 
   return (
     <ProtectedRoute>
       <Tabs
-      backBehavior="history"
+        backBehavior="history"
         screenOptions={{
           headerShown: false,
           tabBarStyle: {
